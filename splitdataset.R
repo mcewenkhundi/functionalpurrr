73dfecb5880696c10fab7f60d7a9ae71b615dabb
mcewenkhundi <40823168+mcewenkhundi@users.noreply.github.com>
2023-08-16 12:47:11 +0200

    added comments to the R script

--- splitdataset.R
+++ splitdataset.R
@@ -3,9 +3,11 @@
 
 library(palmerpenguins)
 
+#Split the penguis dataset by species
 penguins_by_species <- split(x = penguins, f = penguins$species)
 
-#if you do not expect to have values return back, walk is the best map variant of
+#Save each species data into the data folder
+#If you do not expect to have values return back, walk is the best map variant of
 #map
 walk2(penguins_by_species, names(penguins_by_species),
       \(x, y){write_csv(x, file = here::here("data",paste0(y,".csv")))})
